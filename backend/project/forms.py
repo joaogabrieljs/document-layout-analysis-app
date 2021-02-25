0820c9c0f885aca1edd95bf0141624b6d2a30ccc
Joao Gabriel Junqueira <j.g.junqueira@hotmail.com>
2021-02-25 17:36:09 -0300

    Remove unused files

--- backend/project/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-
-from flask_wtf.file import FileField, FileRequired
-from wtforms import SubmitField
-
-
-class ImageUploadForm(FlaskForm):
-    image = FileField()
-    submit = SubmitField(label="submit")
